6b09cd787d84b4b31812dffbee862ed7d99a9472
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-23 22:47:39 +0600

    new

---
+++ src/app/pos/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const POSLayout = async({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default POSLayout
--- src/components/ui/PosNavbar.jsx
+++ src/components/ui/PosNavbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Confirm = () => {
+const PosNavbar = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Confirm = () => {
   )
 }
 
-export default Confirm
+export default PosNavbar

9b3de78d90ccc407c02b38ad3aac43e49ad05776
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-28 02:26:28 +0600

    order

--- src/components/forms/UserOrderForm.jsx
+++ src/components/forms/UserOrderForm.jsx
@@ -2,6 +2,8 @@
 import React, { useContext, useState } from 'react'
 import { Context } from '../context/Context'
 import Link from 'next/link'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const UserOrderForm = () => {
     const { subTotal,  totalPrice,  totalDiscount, cartBar, clearCart, userData, cart } = useContext(Context)
@@ -20,13 +22,33 @@ const UserOrderForm = () => {
             transactionId: '',
             status: 'pending'
         })
+
+        const handleChange=(e)=>{
+            const {name,value}=e.target
+            setFormData((prev)=>({...prev,[name]:value}))
+        }
+        const handleOrder=async(e)=>{
+            try {
+                const res= await axios.post('/api/order', formData,{withCredentials:true})
+                toast.success(res.data.message)
+                clearCart()
+                
+            } catch (error) {
+                toast.error(error?.response?.data?.message || "Failed to place order")
+                
+            }
+        }
   return (
     <div className='w-full flex items-center justify-center'>
         {
             cart.items.length===0? <Link href={'/menu'}>Please select from menu</Link>:
-            <div className='w-auto flex flex-col items-center '>
+            <form className='w-auto flex flex-col items-center '>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id='name' name='name' />
+                </div>
 
-            </div>
+            </form>
         }
       
     </div>
